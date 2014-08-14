a4deff3cdd21a6e146b5a6747e379eaf357cfd5f
Alex Petersen <theoperatore@gmail.com>
2014-08-13 23:40:54 -0500

    updated bounds to use scaled canvas width and height

--- demos/elements/systems/bounds-check.js
+++ demos/elements/systems/bounds-check.js
@@ -2,7 +2,7 @@ exports.name = 'bounds-check';
 exports.reqs = ['position', 'aabb', 'player-controlled'];
 exports.actionEach = function(pkt, entity, position, aabb) {
 
-  var cvs = pkt.firstData('ctx').cvs;
+  var bounds = pkt.firstData('ctx');
 
   if (aabb.left < 0) {
     position.p.x = 5;
@@ -12,8 +12,8 @@ exports.actionEach = function(pkt, entity, position, aabb) {
     aabb.computeAABB();
   }
 
-  if (aabb.right > (cvs.width / 2)) {
-    position.p.x = (cvs.width / 2) - (aabb.width / 2) - 2;
+  if (aabb.right > bounds.width) {
+    position.p.x = bounds.width - (aabb.width / 2) - 2;
     position.v.x *= -0.5;
 
     aabb.anchor = position.p;
@@ -28,8 +28,8 @@ exports.actionEach = function(pkt, entity, position, aabb) {
     aabb.computeAABB();
   }
 
-  if (aabb.bottom > (cvs.height / 2)) {
-    position.p.y = (cvs.height / 2) - (aabb.height / 2) - 2;
+  if (aabb.bottom > bounds.height) {
+    position.p.y = bounds.height - (aabb.height / 2) - 2;
     position.v.y *= -0.5;
 
     aabb.anchor = position.p;
